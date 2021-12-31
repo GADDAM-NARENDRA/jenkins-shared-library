905320300e6e9bf835656f3f4ff1dee3aaa545e1
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2021-12-31 12:25:16 +0530

    Update uatInput.groovy

--- src/com/mcnz/uatInput.groovy
+++ src/com/mcnz/uatInput.groovy
@@ -3,7 +3,7 @@ package com.mcnz
 public class uatInput {
 
   def buildIsUatApproved() {
-    def file = new File("C:/sample/ccc.txt.txt")
+    def file = new File("C:/sample/ccc.txt")
     
     if (file.exists()){
       return true;
